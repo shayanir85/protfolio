54a48258099169c6da2908e84990289a3d435434
shayan iranpour <shayaniranpor8@gmail.com>
2026-07-31 22:01:12 +0330

    fixed a bug

--- BE/app/Filament/Resources/EavEntityResource.php
+++ BE/app/Filament/Resources/EavEntityResource.php
@@ -11,10 +11,10 @@ use Filament\Schemas\Schema;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
-use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Actions\DeleteAction;
-use Filament\Tables\Actions\BulkActionGroup;
-use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
 
 class EavEntityResource extends Resource
 {
--- BE/app/Filament/Resources/ProjectPickResource.php
+++ BE/app/Filament/Resources/ProjectPickResource.php
@@ -8,10 +8,10 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
-use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Actions\DeleteAction;
-use Filament\Tables\Actions\BulkActionGroup;
-use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
@@ -74,4 +74,4 @@ class ProjectPickResource extends Resource
             'edit' => \App\Filament\Resources\ProjectPickResource\Pages\EditProjectPick::route('/{record}/edit'),
         ];
     }
-}
+};
